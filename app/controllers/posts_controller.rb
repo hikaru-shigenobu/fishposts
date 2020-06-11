278f6cc41e45c63158885a7de270b1a6d791a8bf
hikaru.shigenobu <shigenobu_0358@yahoo.co.jp>
2020-06-11 06:10:44 +0000

    fix for posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
   
   def search
