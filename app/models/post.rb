87d2b281321925cfab6d8da09f5bedca6b48c5de
hikaru.shigenobu <shigenobu_0358@yahoo.co.jp>
2020-06-13 11:39:25 +0000

    fix for post validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
     validates :date, presence: true
     validates :time, presence: true
     validates :whether, presence: true
-    validates :fishing_style, presence: true, {lengh: {maximum: 140}}
-    validates :catch, presence: true, {lengh: {maximum: 140}}
+    validates :fishing_style, presence: true, length: { maximum: 150 }
+    validates :catch, presence: true, length: { maximum: 150 }
     validates :report, presence: true, length: { maximum: 3000 }
     validates :address, presence: true, length: { maximum: 50 }
     validates :tide, presence: true
